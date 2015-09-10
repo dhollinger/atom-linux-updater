910667bee57dfcf5abb98bcfc5eca5f84bdd8272
David Hollinger III <david.hollinger@moduletux.com>
2015-09-09 20:12:29 -0500

    add dnf support for fedora 22 and greater

--- atom-update.sh
+++ atom-update.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+OS=`uname -r | cut -d. -f4`
+if [[ "$OS" =~ ^fc2[2-3] ]]; then
+    CMD="dnf"
+else
+    CMD="yum"
+fi
+
 PKG='atom.rpm'
-CURRENT=`yum list installed | grep ^atom | awk '{print $2}' | cut -f1 -d"-"`
+CURRENT=`$CMD list installed | grep ^atom | awk '{print $2}' | cut -f1 -d"-"`
 MASTER=`curl -s https://atom.io/releases | grep release-date | awk '{print $1}' | head -1 | tr -d '<h2>' | tr -d 'v'`
 
 if [ "$MASTER" != "$CURRENT" ]; then
   echo "Downloading Update...."
   wget https://atom.io/download/rpm -O $PKG &> /var/log/aupdater.log
   echo "Installing Update...."
-  yum localinstall -y $PKG &> /var/log/aupdater.log
+  $CMD localinstall -y $PKG &> /var/log/aupdater.log
   echo "Cleanup...."
   rm -rf $PKG &> /var/log/aupdater.log
   echo "Complete!"
