fd34593e323d7de78bbaf7e7a6d06141f5dd7dbe
David Hollinger III <david.hollinger@egov.com>
2015-09-28 09:21:44 -0500

    Add functions for updating packages. Add options.

--- atom-update.sh
+++ atom-update.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 OS=`uname -r | cut -d. -f4`
 if [[ "$OS" =~ ^fc2[2-3] ]]; then
@@ -7,18 +7,41 @@ else
     CMD="yum"
 fi
 
-PKG='atom.rpm'
-CURRENT=`$CMD list installed | grep ^atom | awk '{print $2}' | cut -f1 -d"-"`
-MASTER=`curl -s https://atom.io/releases | grep release-date | awk '{print $1}' | head -1 | tr -d '<h2>' | tr -d 'v'`
+update_atom() {
+    PKG='atom.rpm'
+    CURRENT=`$CMD list installed | grep ^atom | awk '{print $2}' | cut -f1 -d"-"`
+    MASTER=`curl -s https://atom.io/releases | grep release-date | awk '{print $1}' | head -1 | tr -d '<h2>' | tr -d 'v'`
 
-if [ "$MASTER" != "$CURRENT" ]; then
-  echo "Downloading Update...."
-  wget https://atom.io/download/rpm -O $PKG &> /var/log/aupdater.log
-  echo "Installing Update...."
-  $CMD localinstall -y $PKG &> /var/log/aupdater.log
-  echo "Cleanup...."
-  rm -rf $PKG &> /var/log/aupdater.log
-  echo "Complete!"
-else
-  echo "Atom is up to date"
-fi
+    if [ "$MASTER" != "$CURRENT" ]; then
+        echo "Downloading Update...."
+        wget https://atom.io/download/rpm -O $PKG &> /var/log/aupdater.log
+        echo "Installing Update...."
+        $CMD install -y $PKG &> /var/log/aupdater.log
+        echo "Cleanup...."
+        rm -rf $PKG &> /var/log/aupdater.log
+        echo "Complete!"
+    else
+        echo "Atom is up to date"
+    fi
+}
+
+update_packages() {
+    apm update
+}
+
+update_all() {
+    update_atom
+    update_packages
+}
+
+case $1 in
+    atom)
+        update_atom
+        ;;
+    pkg)
+        update_packages
+        ;;
+    *)
+        update_all
+        ;;
+esac
