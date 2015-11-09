a60c58f26ef7bcf5857b5c29377bb53b8b86f3a8
David Hollinger III <david.hollinger@moduletux.com>
2015-11-09 13:23:48 -0600

    Fix version check to remove results with beta

--- atom-update.sh
+++ atom-update.sh
@@ -10,15 +10,15 @@ fi
 update_atom() {
     PKG='atom.rpm'
     CURRENT=`sudo $CMD list installed | grep ^atom | awk '{print $2}' | cut -f1 -d"-"`
-    MASTER=`curl -s https://atom.io/releases | grep release-date | awk '{print $1}' | head -1 | tr -d '<h2>' | tr -d 'v'`
+    MASTER=`curl -s https://atom.io/releases | grep release-date | grep -v beta |awk '{print $1}' | head -1 | tr -d '<h2>' | tr -d 'v'`
 
     if [ "$MASTER" != "$CURRENT" ]; then
         echo "Downloading Update...."
-        wget https://atom.io/download/rpm -O $PKG &> /var/log/aupdater.log
+        wget https://atom.io/download/rpm -O $PKG &> ./aupdater.log
         echo "Installing Update...."
-        sudo $CMD install -y $PKG &> /var/log/aupdater.log
+        sudo $CMD install -y $PKG &> ./aupdater.log
         echo "Cleanup...."
-        rm -rf $PKG &> /var/log/aupdater.log
+        rm -rf $PKG &> ./aupdater.log
         echo "Complete!"
     else
         echo "Atom is up to date"
